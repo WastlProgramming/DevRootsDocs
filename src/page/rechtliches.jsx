b48e58bf737c943188b2d6b3322b315de3c59ec3
Bastian Bichler <bichlerbastian@gmail.com>
2024-08-11 14:26:50 +0200

    added  Asciidoc update ; Sass Devtools Css

--- src/page/rechtliches.jsx
+++ src/page/rechtliches.jsx
@@ -5,8 +5,10 @@ import React from 'react';
 function Rechtliches() {
   return (
     <div>
-      <h1>Rechtliches</h1>
-      <p>Hier sind die rechtlichen Informationen...</p>
+      <h1>Haftungsausschluss</h1>
+      <p>Wir übernehmen keine Verantwortung für die Richtigkeit, Vollständigkeit und Aktualität der bereitgestellten Informationen auf dieser Webseite. Haftungsansprüche gegen uns, die sich auf Schäden materieller oder ideeller Art beziehen, 
+        die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen verursacht wurden, sind grundsätzlich ausgeschlossen.
+        Unser Forum speichert keine Cookies und erhebt keine persönlichen Daten ohne ausdrückliche Zustimmung.</p>
     </div>
   );
 }
